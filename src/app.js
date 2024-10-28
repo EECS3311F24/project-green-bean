bd7881ffa31d83f7cb2d7664e2ecb7d6aed7713b
vishrutkev <kevadiyavishrutd@gmail.com>
2024-10-28 19:45:44 -0400

    testing firebase and restructuring the app

---
+++ src/app.js
@@ -0,0 +1,50 @@
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+const { uploadData } = require("./public/javascripts/setupDB");
+
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
+
+var app = express();
+
+// view engine setup
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+
+// Route to test Firebase upload
+app.get("/test-firebase", async (req, res) => {
+  try {
+    await uploadData(); // Call the uploadData function
+    res.send("Data uploaded successfully to Firebase!");
+  } catch (error) {
+    console.error("Error uploading data to Firebase:", error);
+    res.status(500).send("Failed to upload data to Firebase.");
+  }
+});
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
+  res.status(err.status || 500);
+  res.render("error");
+});
+
+module.exports = app;
