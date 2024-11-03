433e89a14d731615ec2a226d0b0462f77019ca62
orr1nuycruz <orr1n@hotmail.com>
2024-11-03 02:56:29 -0500

    #12[Booking Facilities]: Implement Test Functionality for booking arenas.

--- src/app.js
+++ src/app.js
@@ -9,6 +9,7 @@ var app = express();
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, 'public/stylesheets')));
 
 // initial startup config
 require("./startup/middlewares")(app);
